5c7b2afbbc2e6351ab83f3d545867928e54cecfb
Samsoon Inayat <samsoon.inayat@gmail.com>
2021-10-04 18:09:59 -0600

    -

--- Matlab/common/display_p_table.m
+++ Matlab/common/display_p_table.m
@@ -13,11 +13,11 @@ for rr = 1:size(ptable,1)
         end
         nast = getNumberOfAsterisks(ptable(rr,cc));
         xv = xdata(rr); yv = xdata(cc);
-        text(xv,yv,nast,'FontSize',4,'Color',[0.5 0.5 0.5]);
+        text(xv,yv,nast,'FontSize',5,'Color',[0.5 0.5 0.5]);
     end
 end
 ha1.XColor = [0.5 0.5 0.5]; ha1.YColor = [0.5 0.5 0.5];
-ha1.XAxis.Visible = 'Off'; ha1.FontSize = 4;
+ha1.XAxis.Visible = 'Off'; ha1.FontSize = 5;
 
 vend = xlims(2);
 axes(ha1);cla
@@ -69,12 +69,12 @@ for rr = 1:size(ptable,1)
         end
         nast = getNumberOfAsterisks(ptable(rr,cc));
         xv = lvc(cc); yv = lvc1(rr);%xdata(rr);
-        ht = text(xv,yv,nast,'FontSize',4,'Color',[0.5 0.5 0.5]);
+        ht = text(xv,yv,nast,'FontSize',5,'Color',[0.5 0.5 0.5]);
         ex = ht.Extent;
         nxv = xv - ex(3)/1.5;
         nyv = yv - ex(4)/2;
         delete(ht);
-        ht = text(nxv,yv,nast,'FontSize',4,'Color',[0.5 0.5 0.5]);
+        ht = text(nxv,yv,nast,'FontSize',5,'Color',[0.5 0.5 0.5]);
         n = 0;
     end
 end
