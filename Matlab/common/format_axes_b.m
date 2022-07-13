c8f88da35c953d3ffbe4656f83ecea89b1c2dce1
Samsoon Inayat <samsoon.inayat@gmail.com>
2022-07-13 15:10:01 -0600

    -

--- Matlab/common/format_axes_b.m
+++ Matlab/common/format_axes_b.m
@@ -1,5 +1,6 @@
 function format_axes_b(ha)
 
+ mData = evalin('base','mData');
 if nargin == 1
-    set(ha,'FontSize',6,'FontWeight','Bold','TickDir','out','linewidth',0.25,'xcolor','k','ycolor','k');
+    set(ha,'FontSize',mData.magfac*6,'FontWeight','Bold','TickDir','out','linewidth',0.25,'xcolor','k','ycolor','k');
 end
--- Matlab/common/make_bars_transparent.m
+++ Matlab/common/make_bars_transparent.m
@@ -1,5 +1,5 @@
 function make_bars_transparent(hbs,alphaval)
 
 for ii = 1:length(hbs)
-    set(hbs(ii),'facealpha',alphaval,'EdgeColor','k');
+    set(hbs(ii),'facealpha',alphaval,'EdgeColor',[0.5 0.5 0.5]);
 end
