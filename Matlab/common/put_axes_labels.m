aced2ac3caf9d6664cef8b206b44f01ec2660158
Samsoon Inayat <samsoon.inayat@gmail.com>
2022-07-21 16:44:04 -0600

    -

--- Matlab/common/put_axes_labels.m
+++ Matlab/common/put_axes_labels.m
@@ -1,5 +1,5 @@
-function put_axes_labels(ha,xlab,ylab)
-
+function [hx,hy] = put_axes_labels(ha,xlab,ylab)
+hx = NaN; hy = NaN;
 xlims = get(gca,'xlim');
 ylims = get(gca,'ylim');
 
@@ -7,7 +7,7 @@ tlab = xlab;
 if ~isempty(tlab)
     label = tlab{1};
     pos_l = tlab{2};
-    hc = xlabel(label);
+    hc = xlabel(label); hx = hc;
     if ~isempty(pos_l)
         changePosition(hc,pos_l);
     end
@@ -17,7 +17,7 @@ tlab = ylab;
 if ~isempty(tlab)
     label = tlab{1};
     pos_l = tlab{2};
-    hc = ylabel(label);
+    hc = ylabel(label); hy = hc;
     if ~isempty(pos_l)
         changePosition(hc,pos_l);
     end
